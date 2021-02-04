0fd6cfaaa8adc33ea1f4e5dfaab37498bc4fa6d7
André Dâmaso <andre_damaso_1996@hotmail.com>
2021-02-04 12:20:38 +0000

    update

--- Assets/scripts/TapController.cs
+++ Assets/scripts/TapController.cs
@@ -2,17 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    GameManager game;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+        game = GameManager.Instance;
+        rigidbody.simulated = false;
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnEnable() {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
     }
+
+    void OnDisable() {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted() {
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.simulated = true;
+    }
+
+    void OnGameOverConfirmed() {
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+    void Update() {
+        if(game.GameOver) return;
+
+        if(Input.GetMouseButtonDown(0)){            
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "ScoreZone"){
+            //registe a score event
+            OnPlayerScored(); //event sent to GameManager
+            //play a sound
+        }
+
+        if (col.gameObject.tag == "DeadZone"){
+            rigidbody.simulated = false;
+            //register a dead event
+            OnPlayerDied();
+            //play a sound
+        }
+    }
+
 }
